3c95d686cfbd1935a318dfbf6a9f00103ce70226
Shehzaan Mansuri <shehzaanmansuri1@gmail.com>
2021-12-16 22:19:28 +0530

    CupertinoActivityIndicator

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Cupertino Activity Indicator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CupertinoActivityIndicator(
+        animating: true,
+        radius: 30.0,
+      ),
+    );
+  }
+}
